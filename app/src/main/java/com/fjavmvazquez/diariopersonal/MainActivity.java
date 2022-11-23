749f4fc125e3413bd38605dc05cd8e33a06e956e
fjavmv <fjavmv@gmail.com>
2022-11-23 11:47:44 -0600

    Inicio de proyecto

---
+++ app/src/main/java/com/fjavmvazquez/diariopersonal/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fjavmvazquez.diariopersonal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hola aquí inicia tu proyecto
+        //Suerte
+    }
+}
